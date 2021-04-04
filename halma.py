01cb3ca26a9ab6592702ecd002b0d18a681926bd
Malik Jones <mjdays01@gmail.com>
2021-04-04 09:33:42 -0700

    Create halma.py

---
+++ halma.py
@@ -0,0 +1,17 @@
+# Andrew Munoz 
+# William Fuertes 
+# Uday Bhaskar Savvanna
+# Malik Jones 
+# # # # # # # # # # # 
+
+def boardDisplay():
+  pass 
+
+def updateBoard(): 
+  pass
+
+def move():
+  pass
+
+def winDector():
+  pass
