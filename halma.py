555270393b42fe9cbd67c5b925fbc74bce6cec14
Malik Jones <mjdays01@gmail.com>
2021-04-04 09:36:55 -0700

    Update halma.py

--- halma.py
+++ halma.py
@@ -4,14 +4,20 @@
 # Malik Jones 
 # # # # # # # # # # # 
 
+
+# Graphical board display: Generates nicely-formatted GUI for your system, including the board, plus status and move entry areas
 def boardDisplay():
   pass 
 
+# Board updating: GUI can receive updates to the board, display them smoothly, and includes move highlighting to chose from/to places of last-moved piece.
 def updateBoard(): 
   pass
 
+# Move generator: Given a board and which player to move, it produces a correct and complete list of possible next moves, including jumps. 
+# A “move” method. Takes in a board and a move and returns either a new board (after making move), or an error report detailing a bad move.
 def move():
   pass
 
+# Win detector: Given a board and a player, correctly returns whether it is a winning (or losing!) board for that player.
 def winDector():
   pass
